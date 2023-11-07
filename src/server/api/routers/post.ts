de4a7098f4aaa961ef45615d20c3f2edc1c94af5
bilguungtn <b.baasansuren@gtn.co.jp>
2023-11-07 14:37:22 +0800

    fix

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -5,7 +5,7 @@ import {
   protectedProcedure,
   publicProcedure,
 } from "@/server/api/trpc";
-import { posts } from "@/server/db";
+import { posts } from "@/server/db/schemas";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
--- src/server/db/migrate.ts
+++ src/server/db/migrate.ts
@@ -2,7 +2,7 @@ import { env } from "@/env.mjs";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
-import { Schema } from "./client";
+import { Schema } from "./schemas";
 
 const client = postgres(env.DATABASE_URL, {
   max: 1,
